7b25fe627ef88aeeb374d6f054ed8c7234889a17
Manoj <152408753+MANOJ-39@users.noreply.github.com>
2026-07-03 18:17:19 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
+  root: ".",
   plugins: [react()],
   resolve: {
     alias: {
